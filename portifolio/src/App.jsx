e3b3c014dd5dbc4a6e1f36ac72fdead5cedeae1d
Evilyn Feitosa <evy123feitosa@gmail.com>
2026-08-03 15:24:03 -0300

    Initicial portifolio

---
+++ portifolio/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <main
+    style={{
+      minHeight: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column",
+      fontFamily: "Inter, sans-serif",
+    }}>
+      <h1>Evilyn Feitosa</h1>
+      <p>Data Professional</p>
+      <p>Portifolio under construction.</p>
+    </main>
+  );
+}
+
+export default App;
